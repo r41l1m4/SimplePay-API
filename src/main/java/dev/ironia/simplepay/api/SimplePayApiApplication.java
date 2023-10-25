357fcb0a6746814f10288d104acf4af40f5ad3fa
r41l1m4 <rai.lima17@gmail.com>
2023-10-25 10:30:04 -0300

    Initial commit

---
+++ src/main/java/dev/ironia/simplepay/api/SimplePayApiApplication.java
@@ -0,0 +1,13 @@
+package dev.ironia.simplepay.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ironia/simplepay/api/SimplePayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ironia.simplepay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
